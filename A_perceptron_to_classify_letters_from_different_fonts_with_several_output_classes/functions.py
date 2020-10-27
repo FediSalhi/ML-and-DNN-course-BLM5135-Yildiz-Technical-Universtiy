2f4a2fa18a4d0355ab00ca125a60c4e06f60ff60
FediSalhi <fadi.salhi@outlook.fr>
2020-10-27 21:43:23 +0300

    prepare_dataset() successfully tested

--- A_perceptron_to_classify_letters_from_different_fonts_with_several_output_classes/functions.py
+++ A_perceptron_to_classify_letters_from_different_fonts_with_several_output_classes/functions.py
@@ -3,186 +3,60 @@ import numpy as np
 from global_vars import *
 #########################################
 
-def encode_target(letter, method):
+def prepare_dataset(method):
     """
 
-    :param letter:
+    :param font_type:
     :param method:
     :return:
     """
-    if (method == Target_encode_methods.BINARY):
-        if (letter == 'A'):
-            encoded_target = [1, 0, 0, 0, 0, 0, 0]
 
-        if (letter == 'A'):
-            encoded_target = [0, 1, 0, 0, 0, 0, 0]
+    data_inputs = []
+    data_targets = []
 
-        if (letter == 'A'):
-            encoded_target = [0, 0, 1, 0, 0, 0, 0]
+    letters_list_bipolar = [A_FONT_1_BIPOLAR, B_FONT_1_BIPOLAR, C_FONT_1_BIPOLAR, D_FONT_1_BIPOLAR, E_FONT_1_BIPOLAR,
+                            J_FONT_1_BIPOLAR, K_FONT_1_BIPOLAR, A_FONT_1_BIPOLAR, B_FONT_1_BIPOLAR, C_FONT_1_BIPOLAR,
+                            D_FONT_1_BIPOLAR, E_FONT_1_BIPOLAR, J_FONT_1_BIPOLAR, K_FONT_1_BIPOLAR, A_FONT_1_BIPOLAR,
+                            B_FONT_1_BIPOLAR, C_FONT_1_BIPOLAR, D_FONT_1_BIPOLAR, E_FONT_1_BIPOLAR, J_FONT_1_BIPOLAR,
+                            K_FONT_1_BIPOLAR]
 
-        if (letter == 'A'):
-            encoded_target = [0, 0, 0, 1, 0, 0, 0]
+    targets_list_bipolar = [A_TARGET_BIPOLAR, B_TARGET_BIPOLAR, C_TARGET_BIPOLAR, D_TARGET_BIPOLAR, E_TARGET_BIPOLAR,
+                            J_TARGET_BIPOLAR, A_TARGET_BIPOLAR]
 
-        if (letter == 'A'):
-            encoded_target = [0, 0, 0, 0, 1, 0, 0]
+    # letters_list_bipolar = [A_FONT_1_BINARY, B_FONT_1_BINARY, C_FONT_1_BINARY, D_FONT_1_BINARY, E_FONT_1_BINARY,
+    #                         J_FONT_1_BINARY, K_FONT_1_BINARY, A_FONT_1_BINARY, B_FONT_1_BINARY, C_FONT_1_BINARY,
+    #                         D_FONT_1_BINARY, E_FONT_1_BINARY, J_FONT_1_BINARY, K_FONT_1_BINARY, A_FONT_1_BINARY,
+    #                         B_FONT_1_BINARY, C_FONT_1_BINARY, D_FONT_1_BINARY, E_FONT_1_BINARY, J_FONT_1_BINARY,
+    #                         K_FONT_1_BINARY] #TODO: complete these variables
 
-        if (letter == 'A'):
-            encoded_target = [0, 0, 0, 0, 0, 1, 0]
-
-        if (letter == 'A'):
-            encoded_target = [0, 0, 0, 0, 0, 0, 1]
-
-    if (method == Target_encode_methods.BIPOLAR):
-        if (letter == 'A'):
-            encoded_target = [1, -1, -1, -1, -1, -1, -1]
-
-        if (letter == 'A'):
-            encoded_target = [-1, 1, -1, -1, -1, -1, -1]
-
-        if (letter == 'A'):
-            encoded_target = [-1, -1, 1, -1, -1, -1, -1]
-
-        if (letter == 'A'):
-            encoded_target = [-1, -1, -1, 1, -1, -1, -1]
-
-        if (letter == 'A'):
-            encoded_target = [-1, -1, -1, -1, 1, -1, -1]
-
-        if (letter == 'A'):
-            encoded_target = [-1, -1, -1, -1, -1, 1, -1]
-
-        if (letter == 'A'):
-            encoded_target = [-1, -1, -1, -1, -1, -1, 1]
-
-    return encoded_target
-
-def letter_to_binary(letter, font_type):
-    """
-    :param letter:
-    :param font_type:
-    :return:
-    """
-    if (font_type == Font_types.FONT_1):
-        if (letter == 'A'):
-            A_font_1 = np.array([0, 0, 1, 1, 0, 0, -0,
-                                 0, 0, 0, 1, 0, 0, 0,
-                                 0, 0, 0, 1, 0, 0, 0,
-                                 0, 0, 1, 0, 1, 0, 0,
-                                 0, 0, 1, 0, 1, 0, 0,
-                                 0, 1, 1, 1, 1, 1, 0,
-                                 0, 1, 0, 0, 0, 1, 0,
-                                 0, 1, 0, 0, 0, 1, 0,
-                                 1, 1, 1, 0, 1, 1 ,1])
-
-    #     if (letter == 'B'):
-    #         # TODO: kitap sayfa 72
-    #     if (letter == 'C'):
-    #     if (letter == 'D'):
-    #     if (letter == 'E'):
-    #     if (letter == 'J'):
-    #     if (letter == 'K'):
-    #
-    # if (font_type == font_types.FONT_1):
-    #     if (letter == 'A'):
-    #     if (letter == 'B'):
-    #             # TODO: kitap sayfa 72
-    #     if (letter == 'C'):
-    #     if (letter == 'D'):
-    #     if (letter == 'E'):
-    #     if (letter == 'J'):
-    #     if (letter == 'K'):
-    #
-    # if (font_type == font_types.FONT_1):
-    #     if (letter == 'A'):
-    #     if (letter == 'B'):
-    #             # TODO: kitap sayfa 72
-    #     if (letter == 'C'):
-    #     if (letter == 'D'):
-    #     if (letter == 'E'):
-    #     if (letter == 'J'):
-    #     if (letter == 'K'):
+    targets_list_binary = [A_TARGET_BINARY, B_TARGET_BINARY, C_TARGET_BINARY, D_TARGET_BINARY, E_TARGET_BINARY,
+                            J_TARGET_BINARY, K_TARGET_BINARY]
 
 
-def letter_to_bipolar(letter, font_type):
-    """
-    :param letter:
-    :param font_type:
-    :return:
-    """
-    if (font_type == Font_types.FONT_1):
-        if (letter == 'A'):
-            A_font_1 = np.array([-1, -1, 1, 1, -1, -1, -1,
-                                 -1, -1, -1, 1, -1, -1, -1,
-                                 -1, -1, -1, 1, -1, -1, -1,
-                                 -1, -1, 1, -1, 1, -1, -1,
-                                 -1, -1, 1, -1, 1, -1, -1,
-                                 -1, 1, 1, 1, 1, 1, -1,
-                                 -1, 1, -1, -1, -1, 1, -1,
-                                 -1, 1, -1, -1, -1, 1, -1,
-                                 1, 1, 1, -1, 1, 1 ,1])
+    if (method == Encode_methods.BIPOLAR):
 
-    #     if (letter == 'B'):
-    #         # TODO: kitap sayfa 72
-    #     if (letter == 'C'):
-    #     if (letter == 'D'):
-    #     if (letter == 'E'):
-    #     if (letter == 'J'):
-    #     if (letter == 'K'):
-    #
-    # if (font_type == font_types.FONT_1):
-    #     if (letter == 'A'):
-    #     if (letter == 'B'):
-    #             # TODO: kitap sayfa 72
-    #     if (letter == 'C'):
-    #     if (letter == 'D'):
-    #     if (letter == 'E'):
-    #     if (letter == 'J'):
-    #     if (letter == 'K'):
-    #
-    # if (font_type == font_types.FONT_1):
-    #     if (letter == 'A'):
-    #     if (letter == 'B'):
-    #             # TODO: kitap sayfa 72
-    #     if (letter == 'C'):
-    #     if (letter == 'D'):
-    #     if (letter == 'E'):
-    #     if (letter == 'J'):
-    #     if (letter == 'K'):
+        letters_list = letters_list_bipolar
+        targets_list = targets_list_bipolar
 
-
-def encoding_letter(method, letter, font_type):
-    """
-
-    :param method:
-    :param letter:
-    :param font_type:
-    :return:
-    """
-    if (method == Target_encode_methods.BINARY):
-        encoded_letter = letter_to_binary(letter, font_type)
-
-    if (method == Target_encode_methods.BIPOLAR):
-        encoded_letter = letter_to_bipolar(letter, font_type)
-
-    return encoded_letter
-
-def prepare_data_set(letters_list, method):
-    data_x = [] #inputs
-    data_t = [] #targets
-    fonts_list = [ Font_types.FONT_1,  Font_types.FONT_2,  Font_types.FONT_2]
-
-    # FONT_1
-
-
-    for font in fonts_list:
         for letter in letters_list:
-            encoded_letter = encoding_letter(method, letter, font)
-            encoded_t = encode_target(letter, method)
+            data_inputs.append(letter)
 
-            data_x.append(encoded_letter)
-            data_t.append(encoded_t)
+        for target in targets_list:
+            data_targets.append(target)
 
-    return data_x, data_t
+    # elif (method == Encode_methods.BINARY):
+    #
+    #     letters_list = letters_list_binary
+    #     targets_list = targets_list_binary
+    #
+    #     for letter in letters_list:
+    #         data_inputs.append(letter)
+    #
+    #     for target in targets_list:
+    #         data_targets.append(target)
+
+    return np.array(data_inputs), np.array(data_targets)
+
 
 
 def bipolar_activation(net):
@@ -201,33 +75,34 @@ def binary_activation(net):
 
 
 
-def update_weights_and_biases:
+# def update_weights_and_biases:
     #TODO: from here
 
 
 
-def train_neural_network(epochs, data_x, data_t, learning_rate=1):
-    #TODO: add shuffling option
-    #TODO: add other learning rules
+# def train_neural_network(epochs, data_x, data_t, learning_rate=1):
+#     #TODO: add shuffling option
+#     #TODO: add other learning rules
+#
+#     weights = np.zeros((NUMBER_OF_BITS_PER_LETTER,))
+#     biases = np.zeros((NUMBER_OF_CLASSES, ))
+#
+#     old_weights = np.zeros((NUMBER_OF_BITS_PER_LETTER,))
+#     old_biases = np.zeros((NUMBER_OF_CLASSES,))
+#
+#     for epoch in range(epochs):
+#
+#         for pattern_idx in range(NUMBER_OF_PATTERNS):
+#             net = biases + np.dot(weights, data_x[pattern_idx])
+#             y_out = bipolar_activation(net)
+#
+#             if (y_out != data_t[pattern_idx]):
+#                 weights, biases = update_weights_and_biases(weights, biases, learning_rate, data_t[pattern_idx], data_x[pattern_idx])
+#             else:
+#                 continue
+#
+#         if (weights == old_weights and biases == old_biases):
+#             break
 
-    weights = np.zeros((NUMBER_OF_BITS_PER_LETTER,))
-    biases = np.zeros((NUMBER_OF_CLASSES, ))
-
-    old_weights = np.zeros((NUMBER_OF_BITS_PER_LETTER,))
-    old_biases = np.zeros((NUMBER_OF_CLASSES,))
-
-    for epoch in range(epochs):
-
-        for pattern_idx in range(NUMBER_OF_PATTERNS):
-            net = biases + np.dot(weights, data_x[pattern_idx])
-            y_out = bipolar_activation(net)
-
-            if (y_out != data_t[pattern_idx]):
-                weights, biases = update_weights_and_biases(weights, biases, learning_rate, data_t[pattern_idx], data_x[pattern_idx])
-            else:
-                continue
-
-        if (weights == old_weights and biases == old_biases):
-            break
-
-
+inp, out = prepare_dataset(Encode_methods.BIPOLAR)
+print(inp)
