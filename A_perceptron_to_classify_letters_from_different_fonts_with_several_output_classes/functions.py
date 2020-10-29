7da5297b8ef13f3eedd704915c0a36f8ea91ce31
FediSalhi <fadi.salhi@outlook.fr>
2020-10-29 23:24:43 +0300

    matrix_product(V1, V2) function written and tested

--- A_perceptron_to_classify_letters_from_different_fonts_with_several_output_classes/functions.py
+++ A_perceptron_to_classify_letters_from_different_fonts_with_several_output_classes/functions.py
@@ -1,6 +1,7 @@
 ################ imports ################
 import numpy as np
 from global_vars import *
+import copy
 #########################################
 
 def prepare_dataset(method):
@@ -59,48 +60,53 @@ def prepare_dataset(method):
 
 
 
-def bipolar_activation(net):
-    for idx in range(NUMBER_OF_CLASSES):
-        if net[idx] < 0:
-            net[idx] = -1
-        else:
-            net[idx] = 1
-    return net
-
-def binary_activation(net):
-    for idx in range(NUMBER_OF_CLASSES):
-        if net[idx] < 0:
-            net[idx] = 0
-        else:
-            net[idx] = 1
-    return net
-
-def matrix_product(V1, V2):
-
-    #TODO: not well written, but it works
-
+def activate_bipolar(net):
     """
 
-    :param V1:
-    :param V2:
+    :param net:
     :return:
     """
-    V1 = V1.reshape(-1, 1)
-    V2 = V2.reshape(1, -1)
+    if (net <= 0):
+        activated_net = -1
+    else:
+        activated_net = 1
+    return activated_net
 
-    l1 = V1.shape[0]
-    c1 = V1.shape[1]
 
-    l2 = V2.shape[0]
-    c2 = V2.shape[1]
+def activate_binary(net):
+    """
 
-    V1_cross_V2 = np.zeros((l1, c2))
+    :param net:
+    :return:
+    """
+    if (net <= 0):
+        activated_net = 0
+    else:
+        activated_net = 1
+    return activated_net
 
-    for line in range(l1):
-        for column in range(c2):
-            V1_cross_V2[line, column] = V1[line, 0] * V2[0, column]
 
-    return V1_cross_V2
+
+def matrix_multiplication(M1, M2):
+    result = np.matmul(M1, M2)
+    return result
+
+def  multiply_vectors(V1, V2):
+    """
+    dot multiplication of two vectors
+    :param V1: shape --> (1,n)
+    :param V2: shape --> (n,1)
+    :return: V1.V2  shape --> scaler
+    """
+
+    assert(np.ndim(V1) == 2, "X")
+    assert(np.ndim(V2) == 2, "X")
+    assert(V1.shape[0] == V2.shape[1], "X") #TODO: fill in this message
+    assert(V1.shape[1] == V2.shape[0], "X")
+
+    result = np.dot(V1, V2)
+    return result
+
 
 
 def  update_weights_and_biases(learning_rule, weights, biases, learning_rate, data_input, data_target):
@@ -115,11 +121,22 @@ def  update_weights_and_biases(learning_rule, weights, biases, learning_rate, da
     :return:
     """
     if (learning_rule == Learning_rules.PERCEPTRON):
-        weights += learning_rate * data_target * data_input #TODO: change with cross product
 
-        np.array
+        for output in range(NUMBER_OF_CLASSES):
 
-        biases += learning_rate * data_target
+
+
+
+
+
+
+
+
+        t_multiply_x = matrix_multiplication(data_target.reshape(7, 1), data_input.reshape(1, 63))
+
+        weights += learning_rate * np.transpose(t_multiply_x)
+
+        biases = np.add(biases, (learning_rate * data_target).reshape(7,1))
 
     elif (learning_rule == Learning_rules.DELTA):
         pass
@@ -133,40 +150,62 @@ def train_neural_network(learning_rule, epochs, data_inputs, data_targets, learn
     #TODO: add shuffling option
     #TODO: add other learning rules
 
-    weights = np.zeros((NUMBER_OF_BITS_PER_LETTER * NUMBER_OF_CLASSES,))
-    biases = np.zeros((NUMBER_OF_CLASSES, ))
-
-    old_weights = np.zeros((NUMBER_OF_BITS_PER_LETTER,))
-    old_biases = np.zeros((NUMBER_OF_CLASSES,))
+    weights = np.zeros((NUMBER_OF_BITS_PER_LETTER, NUMBER_OF_CLASSES))
+    biases = np.zeros((NUMBER_OF_CLASSES, 1))
 
     for epoch in range(epochs):
 
-        old_weights = weights
-        old_biases = biases
+        old_weights = copy.copy(weights)
+        old_biases = copy.copy(biases)
+
 
         for pattern_idx in range(NUMBER_OF_LETTERS_PER_FONT * NUMBER_OF_FONTS):
-            net = biases + np.dot(weights, data_inputs[pattern_idx])
-            y_out = bipolar_activation(net)
+            x = data_inputs[pattern_idx].reshape(1, 63)
+            activated_output = []
+            # Compute activation for each output unit
+            for output_idx in range(NUMBER_OF_CLASSES):
+                wj = weights[:, output_idx]
+                bj = biases[output_idx]
+                net = compute_net(x, wj, bj)
+                yj = activate_bipolar(net)
+                activated_output.append(yj)
 
-            if ((y_out != data_targets[pattern_idx % 7]).any()):
+            target_j = data_targets[pattern_idx % 7]
+
+           #TODO: here
+            # Update biases and weights
+            for output_idx in range(NUMBER_OF_CLASSES):
+                if (target_j[output_idx] != activated_output[output_idx]):
+                    w_old = weights[:, output_idx]
+                    b_old = biases[output_idx]
+                    b_new, w_new = update_weights_and_biases(Learning_rules.PERCEPTRON, learning_rate, b_old, w_old, target_j, x, )
+
+            if (~np.array_equal(y_out, data_targets[pattern_idx % 7].reshape(7,1))):
                 weights, biases = update_weights_and_biases(Learning_rules.PERCEPTRON, weights, biases, learning_rate, data_inputs[pattern_idx], data_targets[pattern_idx % 7])
             else:
                 continue
 
-        # if ((weights == old_weights).any() and (biases == old_biases).any()):
-        #     return weights, biases
-
+        if (np.array_equal(weights, old_weights) and np.array_equal(biases, old_biases)):
+            print(epoch)
+            return weights, biases
+    print(epoch)
     return weights, biases
 
 
-# data_inputs, data_targets = prepare_dataset(Encode_methods.BIPOLAR)
-#
-# weights, biases = train_neural_network(1, 100, data_inputs, data_targets, 1)
-#
-# print(weights)
-# print(biases)
+data_inputs, data_targets = prepare_dataset(Encode_methods.BIPOLAR)
 
-x = cross_product(np.array([1,2,3]), np.array([6,1,9]))
-print(x)
+weights, biases = train_neural_network(1, 100, data_inputs, data_targets, 1)
+
+print(weights.shape)
+print(biases.shape)
+
+print(weights)
+print(biases)
+
+
+
+a = np.array([1,2])
+np.ndim
+
 
 
