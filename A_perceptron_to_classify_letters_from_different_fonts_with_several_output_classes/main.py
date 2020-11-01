df64567ee755daf21585f53747d48d270fbb59a0
FediSalhi <fadi.salhi@outlook.fr>
2020-11-01 10:10:16 +0300

    binary letters updated

--- A_perceptron_to_classify_letters_from_different_fonts_with_several_output_classes/main.py
+++ A_perceptron_to_classify_letters_from_different_fonts_with_several_output_classes/main.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
 
     data_inputs, data_targets = prepare_dataset(LETTERS_ENCODING_METHOD)
     weights, biases = train_neural_network(LEARNING_RULE, data_inputs, data_targets, LEARNING_RATE)
-    prediction = get_prediction(weights, biases, np.array(B_FONT_2_BIPOLAR))
+    prediction = get_prediction(weights, biases, np.array(K_FONT_1_BIPOLAR))
     print(prediction)
