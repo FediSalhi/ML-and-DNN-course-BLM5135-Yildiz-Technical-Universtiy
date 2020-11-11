c6fc1690d4ea1946e3b0c9142ef12aae5c3eb41a
FediSalhi <fadi.salhi@outlook.fr>
2020-11-11 22:50:31 +0300

    verify_chosen_point function defined

--- Steepest descent example/all_functions.py
+++ Steepest descent example/all_functions.py
@@ -12,7 +12,7 @@ def f_function_grad(x1, x2):
     return grad_f
 
 def compute_norm(X):
-    norm = np.sqrt(X[0]*X[0] + X[0]*X[0])
+    norm = np.sqrt(X[0]*X[0] + X[1]*X[1])
     return norm
 
 def verify_min_point_or_not(X0):
@@ -23,4 +23,17 @@ def verify_min_point_or_not(X0):
     else:
         result = False
     return result
+
+def verify_chosen_point(X1, X2):
+    diff_points = [X1[0] - X2[0], X1[1] - X2[1]]
+    distance_points = compute_norm(diff_points)
+
+    diff_func = f_function(X1[0], X1[1]) - f_function(X2[0], X2[1])
+    abs_diff_func = abs(diff_func)
+    if (distance < EPSILON_2 and abs_diff_func < EPSILON_2):
+        chosen_point_verified = True
+    else:
+        chosen_point_verified = False
+
+    return chosen_point_verified
 
