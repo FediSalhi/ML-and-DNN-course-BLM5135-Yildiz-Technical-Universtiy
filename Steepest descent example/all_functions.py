a364e903dcab78a258cefe7e0a8b69c8b0c6222d
FediSalhi <fadi.salhi@outlook.fr>
2020-11-11 22:24:29 +0300

    f_function defined

---
+++ Steepest descent example/all_functions.py
@@ -0,0 +1,10 @@
+################ imports ################
+import numpy as np
+#########################################
+
+def f_function(x1, x2):
+    f = 2*x1*x1 + x1*x2 + x2*x2
+    return f
+
+def f_function_grad(x1, x2):
+    grad_f =
