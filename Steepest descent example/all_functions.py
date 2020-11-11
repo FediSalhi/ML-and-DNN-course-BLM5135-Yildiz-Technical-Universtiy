ad0dbccdd21e8f55d83430bde0b8e2cebc686e80
FediSalhi <fadi.salhi@outlook.fr>
2020-11-11 22:38:26 +0300

    verify_min_point_or_not() created

--- Steepest descent example/all_functions.py
+++ Steepest descent example/all_functions.py
@@ -1,5 +1,6 @@
 ################ imports ################
 import numpy as np
+from constants import *
 #########################################
 
 def f_function(x1, x2):
@@ -9,4 +10,17 @@ def f_function(x1, x2):
 def f_function_grad(x1, x2):
     grad_f = [4*x1 + x2, x1 + 2*x2]
     return grad_f
+
+def compute_norm(X):
+    norm = np.sqrt(X[0]*X[0] + X[0]*X[0])
+    return norm
+
+def verify_min_point_or_not(X0):
+    grad = f_function_grad(X0[0], X0[1])
+    norm_grad = compute_norm(grad)
+    if (norm_grad < EPSILON_1):
+        result = True
+    else:
+        result = False
+    return result
 
